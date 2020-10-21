880d1f38c2cadc26adc048faea17b14c2bfe4426
leal <leal.liu@kuchain.io>
2020-10-21 15:28:14 +0800

    merge v0.5.2

--- make.sh
+++ make.sh
@@ -4,6 +4,6 @@ GOARCH=amd64 \
 go build -mod=readonly \
 -ldflags "-X github.com/KuChainNetwork/kuchain/chain/constants/keys.ChainNameStr=kts -X github.com/KuChainNetwork/kuchain/chain/constants/keys.ChainMainNameStr=kratos" \
 -o kucd github.com/KuChainNetwork/kuchain/cmd/kucd && \
-upx kucd && \
-scp kucd root@ks02:/data/db_backend && \
-ssh -t root@ks02 "cd /data/db_backend && ./bi.sh && docker-compose down && docker-compose up -d"
+upx kucd
+#scp kucd root@ks02:/data/db_backend && \
+#ssh -t root@ks02 "cd /data/db_backend && ./bi.sh && docker-compose down && docker-compose up -d"
